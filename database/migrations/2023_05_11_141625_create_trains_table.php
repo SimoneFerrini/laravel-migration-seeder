18b3472ddd7d18d82256f6dd5ccccc8cf484030a
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-11 17:08:57 +0200

    collegamenti model controller e prova

--- database/migrations/2023_05_11_141625_create_trains_table.php
+++ database/migrations/2023_05_11_141625_create_trains_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
 
-            $table->string('azienda');
-            $table->string('stazione_di_partenza');
-            $table->string('Stazione_di_arrivo');
-            $table->time('orario_di_partenza');
-            $table->time('orario_di_arrivo');
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
             $table->smallInteger('codice_treno');
             $table->tinyInteger('numero_carrozze');
             $table->boolean('in_orario');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts/main')
 
 @section('content')
-    ciao
+    <table>
+        <thead>
+            
+            <th>Azienda</th>
+            <th>Stazione di partenza</th>
+            <th>Stazione di arrivo</th>
+            <th>Orario di partenza</th>
+            <th>Orario di arrivo</th>
+            <th>Codice treno</th>
+            <th>Numero Carrozze</th>
+            <th>In orario</th>
+            <th>Cancellato</th>
+        </thead>
+        <tbody>
+            @foreach ($treni as $treno)
+            <tr>
+                <td>{{$treno->azienda}}</td>
+                <td>{{$treno->stazione_di_partenza}}</td>
+                <td>{{$treno->Stazione_di_arrivo}}</td>
+                <td>{{$treno->orario_di_partenza}}</td>
+                <td>{{$treno->orario_di_arrivo}}</td>
+                <td>{{$treno->codice_treno}}</td>
+                <td>{{$treno->numero_carrozze}}</td>
+                <td>{{$treno->in_orario}}</td>
+                <td>{{$treno->cancellato}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
